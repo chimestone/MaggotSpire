19d48b0c1df98fbc57661c74a201b5f0159e7aee
yone <2424850182@qq.com>
2026-09-06 22:54:29 +0800

    Integrate RitsuLib content discovery

--- src/Entry.cs
+++ src/Entry.cs
@@ -1,5 +1,9 @@
 using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Modding;
+using STS2RitsuLib;
+using System.Reflection;
+using STS2RitsuLib.Interop;
+
 
 namespace MaggotSpire;
 
@@ -8,7 +12,16 @@ public static class Entry
 {
     public static void Initialize()
     {
+        var assembly = Assembly.GetExecutingAssembly();
+        RitsuLibFramework.EnsureGodotScriptsRegistered(assembly, Logger);
+        ModTypeDiscoveryHub.RegisterModAssembly(ModId, assembly);
+        
         Log.Info("[MaggotSpire] Mod initialized successfully.");
     }
+
+    public const string ModId = "MaggotSpire";
+
+    public static readonly Logger Logger =
+    RitsuLibFramework.CreateLogger(ModId);
 }
 
